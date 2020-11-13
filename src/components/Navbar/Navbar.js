24084292f33b114cad712f0df682f6028eafe149
Lavrentev Stanislav <hvoinii@gmail.com>
2020-11-13 17:24:14 +0600

    create NavMenu, NavItem, NavLinks components for Burger menu; change left css option when click

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,14 @@
 import React, { useState } from 'react';
-import { Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon } from './Navbar.elements';
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  NavIcon,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks
+} from './Navbar.elements';
 import { FaTimes, FaBars } from 'react-icons/fa';
 import { IconContext } from 'react-icons/lib';
 
@@ -20,6 +29,17 @@ const Navbar = () => {
             <MobileIcon onClick={handleClick}>
               {click ? <FaTimes /> : <FaBars />}
             </MobileIcon>
+            <NavMenu onClick={handleClick} click={click}>
+              <NavItem>
+                <NavLinks to='/'>Home</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to='/services'>Services</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to='/products'>Products</NavLinks>
+              </NavItem>
+            </NavMenu>
           </NavbarContainer>
         </Nav>
       </IconContext.Provider>
