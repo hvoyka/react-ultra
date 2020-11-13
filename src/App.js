ae72b27a6877b1ad67faf1789fa30f40a1dbc745
Lavrentev Stanislav <hvoinii@gmail.com>
2020-11-13 12:18:22 +0600

    create index.js in components, to import default all components; create first component with styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 
+import React from 'react';
+import {Navbar} from './components';
+
 function App() {
   return (
     <div className="App">
-     <h1>HO!</h1>
+     <Navbar/>
     </div>
   );
 }
