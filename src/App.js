41f0231584ca8283edafc68ba990fb85af01e931
Lavrentev Stanislav <hvoinii@gmail.com>
2020-11-14 12:56:41 +0600

    create InfoSection component, and some small components in it; create Data.js with settings for this component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 
 import React from 'react';
 import GlobalStyle from './globalStyles'
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import {Navbar} from './components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navbar } from './components';
+import Home from './pages/HomePage/Home';
 
 function App() {
   return (
     <Router>
-      <GlobalStyle/>
-        <Navbar/> 
+      <GlobalStyle />
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home}/>
+      </Switch>
     </Router>
   );
 }
