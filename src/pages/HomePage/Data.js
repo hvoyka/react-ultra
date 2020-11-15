58ba1e839578df689aad8637e104426b98636ec9
Lavrentev Stanislav <hvoinii@gmail.com>
2020-11-15 15:07:55 +0600

    add sections, change options for it; some fixes

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -14,3 +14,53 @@ export const homeObjOne = {
   alt: 'Image',
   start: '' 
 }
+
+export const homeObjTwo = {
+  light: false,
+  primary: false,
+  lightBg: true,
+  imgStart: 'start',
+  lightTopLine: false,
+  lightTextDesc: false,
+  lightText: false,
+  buttonLabel: 'Get Started',
+  description: 'We help business owners increase their revenue. Our team of unique specialist can help you acheive youre business goals.',
+  headline: 'Lead Generation Specialist for Online Businesses',
+  topLine: 'Margeting Agency',
+  img: require('../../images/profile.jpg').default,
+  alt: 'Image',
+  start: 'true' 
+}
+
+export const homeObjThree = {
+  light: false,
+  primary: true,
+  lightBg: false,
+  imgStart: '',
+  lightTopLine: true,
+  lightTextDesc: true,
+  lightText: true,
+  buttonLabel: 'Get Started',
+  description: 'We help business owners increase their revenue. Our team of unique specialist can help you acheive youre business goals.',
+  headline: 'Lead Generation Specialist for Online Businesses',
+  topLine: 'Margeting Agency',
+  img: require('../../images/svg-2.svg').default,
+  alt: 'Image',
+  start: '' 
+}
+export const homeObjFour = {
+  light: false,
+  primary: true,
+  lightBg: false,
+  imgStart: 'start',
+  lightTopLine: true,
+  lightTextDesc: true,
+  lightText: true,
+  buttonLabel: 'Get Started',
+  description: 'We help business owners increase their revenue. Our team of unique specialist can help you acheive youre business goals.',
+  headline: 'Lead Generation Specialist for Online Businesses',
+  topLine: 'Margeting Agency',
+  img: require('../../images/svg-3.svg').default,
+  alt: 'Image',
+  start: 'true' 
+}
