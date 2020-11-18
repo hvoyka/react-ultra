8520fbc31bb79cd4ef0b7d8a4ff6a2ffb6c3de3a
Lavrentev Stanislav <hvoinii@gmail.com>
2020-11-18 20:32:33 +0600

    add subscribe form to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {FooterContainer, FooterSubscription, FooterSubHeading, FooterSubText} from './Footer.elements';
+import { Button } from '../../globalStyles';
+import { FooterContainer, FooterSubscription, FooterSubHeading, FooterSubText, Form, FormInput } from './Footer.elements';
 
 const Footer = () => {
   return (
@@ -9,6 +10,10 @@ const Footer = () => {
           Join our exclusive membership to receive the latest news and trends
         </FooterSubHeading>
         <FooterSubText>You can unsubcribe at any time</FooterSubText>
+        <Form>
+          <FormInput name="email" type="email" placeholder="Your Email" />
+          <Button fontBig>Subscribe</Button>
+        </Form>
       </FooterSubscription>
     </FooterContainer>
   )
