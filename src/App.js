42600083021f03ad1ab8fc5a76823b9d1c62735f
Lavrentev Stanislav <hvoinii@gmail.com>
2020-11-18 20:16:28 +0600

    create Footer, and styls for it

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import GlobalStyle from './globalStyles'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Navbar } from './components';
+import { Navbar, Footer } from './components';
 import Home from './pages/HomePage/Home';
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
       <Switch>
         <Route path='/' exact component={Home}/>
       </Switch>
+      <Footer/>
     </Router>
   );
 }
