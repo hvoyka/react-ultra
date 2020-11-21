c31b77e9907f3b800185621e92beb4ccf5179780
Lavrentev Stanislav <hvoinii@gmail.com>
2020-11-21 20:03:50 +0600

    Footer change links name; fix FooterLinksWrapper position

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin} from 'react-icons/fa'
+import { FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin } from 'react-icons/fa'
 import { Button } from '../../globalStyles';
-import { FooterContainer, 
-  FooterSubscription, 
-  FooterSubHeading, 
-  FooterSubText, 
-  Form, 
+import {
+  FooterContainer,
+  FooterSubscription,
+  FooterSubHeading,
+  FooterSubText,
+  Form,
   FormInput,
   FooterLinksContainer,
   FooterLinksWrapper,
@@ -46,52 +47,51 @@ const Footer = () => {
           </FooterLinkItems>
           <FooterLinkItems>
             <FooterLinkTitle>Contact us</FooterLinkTitle>
-            <FooterLink to='/sign-up'>How it works</FooterLink>
-            <FooterLink to='/'>Testimonials</FooterLink>
-            <FooterLink to='/'>Careers</FooterLink>
-            <FooterLink to='/'>Investors</FooterLink>
-            <FooterLink to='/'>Terms of Service</FooterLink>
+            <FooterLink to='/'>Contact</FooterLink>
+            <FooterLink to='/'>Support</FooterLink>
+            <FooterLink to='/'>Destinations</FooterLink>
+            <FooterLink to='/'>SponsorShips</FooterLink>
           </FooterLinkItems>
+        </FooterLinksWrapper>
+        <FooterLinksWrapper>
           <FooterLinkItems>
             <FooterLinkTitle>Videos</FooterLinkTitle>
-            <FooterLink to='/sign-up'>How it works</FooterLink>
-            <FooterLink to='/'>Testimonials</FooterLink>
-            <FooterLink to='/'>Careers</FooterLink>
-            <FooterLink to='/'>Investors</FooterLink>
-            <FooterLink to='/'>Terms of Service</FooterLink>
+            <FooterLink to='/'>Submit Video</FooterLink>
+            <FooterLink to='/'>Ambassadors</FooterLink>
+            <FooterLink to='/'>Agency</FooterLink>
+            <FooterLink to='/'>Influencer</FooterLink>
           </FooterLinkItems>
           <FooterLinkItems>
             <FooterLinkTitle>Social Media</FooterLinkTitle>
-            <FooterLink to='/sign-up'>How it works</FooterLink>
-            <FooterLink to='/'>Testimonials</FooterLink>
-            <FooterLink to='/'>Careers</FooterLink>
-            <FooterLink to='/'>Investors</FooterLink>
-            <FooterLink to='/'>Terms of Service</FooterLink>
+            <FooterLink to='/'>Instagram</FooterLink>
+            <FooterLink to='/'>Facebook</FooterLink>
+            <FooterLink to='/'>Youtube</FooterLink>
+            <FooterLink to='/'>Twitter</FooterLink>
           </FooterLinkItems>
         </FooterLinksWrapper>
       </FooterLinksContainer>
       <SocialMedia>
         <SocialMediaWrap>
           <SocialLogo to='/'>
-            <SocialIcon/>
+            <SocialIcon />
             ULTRA
           </SocialLogo>
           <WebsiteRight>ULTRA &copy; 2020</WebsiteRight>
           <SocialIcons>
             <SocialIconLink href='/' target='_blank' aria-labe='FaceBook'>
-              <FaFacebook/>
+              <FaFacebook />
             </SocialIconLink>
             <SocialIconLink href='/' target='_blank' aria-labe='Instagram'>
-              <FaInstagram/>
+              <FaInstagram />
             </SocialIconLink>
             <SocialIconLink href='/' target='_blank' aria-labe='Youtube'>
-              <FaYoutube/>
+              <FaYoutube />
             </SocialIconLink>
             <SocialIconLink href='/' target='_blank' aria-labe='Twitter'>
-              <FaTwitter/>
+              <FaTwitter />
             </SocialIconLink>
             <SocialIconLink href='/' target='_blank' aria-labe='LinkedIn'>
-              <FaLinkedin/>
+              <FaLinkedin />
             </SocialIconLink>
           </SocialIcons>
         </SocialMediaWrap>
